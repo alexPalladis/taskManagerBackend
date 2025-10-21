a848e2f4311a6615b8edd98564930fbaa61aea84
alex_Palladis <alehandro4443@gmail.com>
2025-10-21 21:29:58 +0300

    enable cors options

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ const { errorHandler, notFound } = require("./middlewares/errorHandler");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/tasks", taskRoutes);
